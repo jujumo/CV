a22b9ab39cb305e69bf1ec11c5dd79411c15be3f
jumo <ju.jumo@gmail.com>
2019-02-04 19:56:50 +0100

    resources

---
+++ resources/Autopano Video - Stereo (3D) - Autopano_files/load_006.php
@@ -0,0 +1,3 @@
+$('#myTab.nav a').click(function(e){e.preventDefault()
+$(this).tab('show')});mw.loader.state({"site":"ready"});
+/* cache key: kolor_mediawiki-new-en_:resourceloader:filter:minify-js:7:e93495985bdb30065fbac875be65f508 */
